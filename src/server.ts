f3045f06782d37bd07839d5be4770b7f360fe15d
Raul Mendes Rosá <raul.hmetal@gmail.com>
2021-01-11 22:58:24 -0300

    Created database connection

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import router from './routes';
 
+import './database';
+
 const app = express();
 
+app.use(express.json());
 app.use(router);
 
 app.listen(5000, () => console.log('Server running'));
