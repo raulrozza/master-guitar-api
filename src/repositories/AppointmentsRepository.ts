8ae27ab6f522312417fa7a81f33a96964ca85a3a
Raul Mendes Rosá <raul.hmetal@gmail.com>
2021-01-13 22:03:47 -0300

    Created Appointments service

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,17 @@
+import { EntityRepository, Repository } from 'typeorm';
+import Appointment from '../models/Appointment';
+
+@EntityRepository(Appointment)
+export class AppointmentsRepository extends Repository<Appointment> {
+    public async findByDate(date: Date): Promise<Appointment | null> {
+        const appointment = await this.findOne({
+            where: {
+                date,
+            },
+        });
+
+        return appointment || null;
+    }
+}
+
+export default AppointmentsRepository;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import appointmentRoutes from './appointment.routes';
 
 const router = Router();
+
+router.use('/appointments', appointmentRoutes);
 
 export default router;
