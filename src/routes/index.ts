1908702ef76da40c671e0a61f61b51fd19f0c4e0
Raul Mendes Rosá <raul.hmetal@gmail.com>
2021-01-18 20:11:01 -0300

    Created user sessions

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import appointmentRoutes from './appointment.routes';
+import sessionsRouter from './sessions.routes';
 import usersRouter from './users.routes';
 
 const router = Router();
 
 router.use('/appointments', appointmentRoutes);
+router.use('/sessions', sessionsRouter);
 router.use('/users', usersRouter);
 
 export default router;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,4 +1,3 @@
-import { parseISO } from 'date-fns';
 import { Router } from 'express';
 import CreateUserService from '../services/CreateUserService';
 
