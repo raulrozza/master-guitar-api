4cd9a3f14baef3c0aa71cec0fb3c54c2e2e0a434
Raul Mendes Rosá <raul.hmetal@gmail.com>
2021-01-20 22:04:43 -0300

    Statically serving files

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,14 @@ import 'reflect-metadata';
 import express from 'express';
 import router from './routes';
 
+import uploadConfig from './config/upload';
+
 import './database';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(router);
 
 app.listen(5000, () => console.log('Server running'));
