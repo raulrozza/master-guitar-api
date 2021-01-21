82eda61637930641abeab938284c21058fd7cc57
Raul Mendes Rosá <raul.hmetal@gmail.com>
2021-01-20 22:32:09 -0300

    Error Handling

--- src/routes/appointment.routes.ts
+++ src/routes/appointment.routes.ts
@@ -17,22 +17,18 @@ appointmentRoutes.get('/', async (_, response) => {
 });
 
 appointmentRoutes.post('/', async (request, response) => {
-    try {
-        const { provider_id, date } = request.body;
+    const { provider_id, date } = request.body;
 
-        const parsedDate = parseISO(date);
+    const parsedDate = parseISO(date);
 
-        const createAppointment = new CreateAppointmentService();
+    const createAppointment = new CreateAppointmentService();
 
-        const appointment = await createAppointment.execute({
-            date: parsedDate,
-            provider_id,
-        });
+    const appointment = await createAppointment.execute({
+        date: parsedDate,
+        provider_id,
+    });
 
-        return response.json(appointment);
-    } catch (error) {
-        return response.status(400).json({ error: error.message });
-    }
+    return response.json(appointment);
 });
 
 export default appointmentRoutes;
