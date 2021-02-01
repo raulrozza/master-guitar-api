5d06ea7f130df776a1ef66d0eb4d613c7a36d0c0
Raul Mendes Rosá <raul.hmetal@gmail.com>
2021-02-01 15:29:53 -0300

    Added cors

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 
+import cors from 'cors';
+
 import router from './routes';
 
 import uploadConfig from './config/upload';
@@ -11,6 +13,8 @@ import './database';
 import AppError from './errors/AppError';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
